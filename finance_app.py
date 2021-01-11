808ad7611a9796f6e072b41892818f4f63ac7bd6
Wisal Mukhtiar <wisalmukhtiar7@gmail.com>
2021-01-11 12:19:42 +0500

    Did the first commit

---
+++ finance_app.py
@@ -0,0 +1,45 @@
+class Bill:
+    def __init__(self, bill_detail, spent):
+        self.bill_detail=bill_detail
+        self.spent=spent
+        self.breakfast=[]
+        self.lunch=[]
+        self.dinner=[]
+        self.totalspent=0
+        self.available=[]
+        self.extra=[]
+    
+        if self.bill_detail=="breakfast" or self.bill_detail=="BreakFast":
+            self.breakfast.append(spent)        
+        
+        elif self.bill_detail=="dinner" or self.bill_detail=="Dinner":
+            self.breakfast.append(spent)        
+        
+        elif self.bill_detail=="=Lunch" or self.bill_detail=="Lunch":
+            self.breakfast.append(spent)        
+        
+        else:
+            self.extra.append(spent)
+
+
+    def __repr__(self):
+        return f"< Bill({self.bill_detail}, {self.spent}) >"  
+    
+    def spent_amount(self):
+        for amount in self.lunch:
+            self.totalspent+=amount
+
+        for amount in self.breakfast:
+            self.totalspent+=amount
+        
+        for amount in self.dinner:
+            self.totalspent+=amount
+        
+        for amount in self.extra:
+            self.totalspent+=amount
+        
+    def show_total_spent(self):
+        return f"Total amount spent ==> {self.totalspent}"    
+
+
+#Yet to be
